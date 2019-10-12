f4bf42dc0aeef97e79c1f7170c3d3e5fcdf061ea
Frank Caron <frank@frankcaron.com>
2019-10-12 02:22:15 -0400

    FML

--- public/helper.js
+++ public/helper.js
@@ -10,4 +10,6 @@ function onLogout() {
 var identity = localStorage.getItem(localStorage.key(0));
 if (identity != null) {
     document.write("Identity is " + identity);
+} else {
+    document.write("Looks like the localStorage is empty because there's nothing at " + localStorage.key(0));
 }
