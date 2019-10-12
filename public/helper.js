bbed05d0fa7155111c06fb26504c49b5266fd665
Frank Caron <frank@frankcaron.com>
2019-10-12 19:30:44 -0400

    Tweaked front end code

--- public/helper.js
+++ public/helper.js
@@ -1,27 +1,26 @@
 function onLogin(identity) {
     //responseJSON = JSON.parse(identity);
-    console.log("Identity = " + identity.dump);
+    console.log("Identity = " + identityServiceResponse);
 
 		
     var targetDiv = document.querySelector(SFIDWidget.config.target);	
     
     var avatar = document.createElement('a'); 
     avatar.href = "javascript:showIdentityOverlay();";
-    
-    
-    var img = document.createElement('img'); 
-    img.src = identity.photos.thumbnail; 
-    img.className = "sfid-avatar";
 
     var username = document.createElement('span'); 
     username.innerHTML = identity.username;
     username.className = "sfid-avatar-name";
 
+    var img = document.createElement('img'); 
+    img.src = identity.photos.thumbnail; 
+    img.className = "sfid-avatar";
+
     var iddiv = document.createElement('div'); 
     iddiv.id = "sfid-identity";
-    
-    avatar.appendChild(img);
+
     avatar.appendChild(username);
+    avatar.appendChild(img);
     iddiv.appendChild(avatar);		
 
     targetDiv.innerHTML = '';
