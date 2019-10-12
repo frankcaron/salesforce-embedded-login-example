d52165e4fb86be9408e3e27525617c833c14ef1d
Frank Caron <frank@frankcaron.com>
2019-10-12 02:03:32 -0400

    Now we're getting somewhere

--- public/helper.js
+++ public/helper.js
@@ -5,3 +5,9 @@ function onLogin(identity) {
 function onLogout() {
     SFIDWidget.init();
 }
+
+//* Get value from local storage *//
+var identity = localStorage.getItem('identity');
+if (identity != null) {
+    document.write("Identity is " + identity);
+}
