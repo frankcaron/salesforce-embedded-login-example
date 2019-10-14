8508127ad36cc92d21df7a7c59d144c8d6ab3b13
Frank Caron <frank@frankcaron.com>
2019-10-14 11:30:24 -0400

    Added searches for records

--- index.js
+++ index.js
@@ -45,8 +45,14 @@ app.get('/profile', function(req, res){
 
     console.log("Profile Render: Fetching profile information...")
 
+    //Grab Contact
     var contactRecords = [];
-    conn.query("SELECT Id, Name FROM Contact WHERE Id = '" + sessionContact + "'", function(err, result) {
+    var bookingRecords = [];
+    var searchRecords = [];
+    var wishes = [];
+
+    //Grab Contact
+    conn.query("SELECT Id, FirstName, LastName, Phone, Email, customerID__c FROM Contact WHERE Id = '" + sessionContact + "'", function(err, result) {
         if (err) { return console.error(err); }
         console.log("Profile Render: Contact result size is " + result.totalSize);
         console.log("Profile Render: Number of contacts found is " + result.records.length);
@@ -54,17 +60,54 @@ app.get('/profile', function(req, res){
         contactRecords = result.records;
         console.log("Profile Render: Contact retrieved " + JSON.stringify(contactRecords));
 
-        //Render the page once records are fetched
-        res.render('profile', {
-            community_url: COMMUNITY_URL,
-            app_id: APP_ID,
-            callback_url: OAUTH_CALLBACK_URL,
-            background: BG_FAKE,
-            static_asset_url: STATIC_ASSET_URL,
-            contactRecords: contactRecords
-        }) 
+        //Grab Bookings
+        conn.query("SELECT customerID__c,DisplayUrl,ExternalId,orderDate__c,orderID__c,shippedDate__c FROM Bookings__x WHERE customerID__c = " + contactRecords[0].customerID__c + "'", function(err, result) {
+            if (err) { return console.error(err); }
+            console.log("Profile Render: Bookings result size is " + result.totalSize);
+            console.log("Profile Render: Number of bookings found is " + result.records.length);
+    
+            bookingRecords = result.records;
+            console.log("Profile Render: Bookings retrieved " + JSON.stringify(bookingRecords));
+    
+            //Grab Wishlist
+            conn.query("SELECT Contact__c,CreatedDate,Id,Wish_Detail__c FROM Wish__c WHERE Contact__c = '" + sessionContact + "'", function(err, result) {
+                if (err) { return console.error(err); }
+                console.log("Profile Render: Wishlist result size is " + result.totalSize);
+                console.log("Profile Render: Number of wishes found is " + result.records.length);
+        
+                wishes = result.records;
+                console.log("Profile Render: wishes retrieved " + JSON.stringify(wishes));
+
+                //Grab Searches
+                conn.query("SELECT Contact__c,CreatedDate,Id,Location__c FROM Searches__c WHERE Contact__c = '" + sessionContact + "'", function(err, result) {
+                    if (err) { return console.error(err); }
+                    console.log("Profile Render: Search result size is " + result.totalSize);
+                    console.log("Profile Render: Number of searches found is " + result.records.length);
+            
+                    searchRecords = result.records;
+                    console.log("Profile Render: Searches retrieved " + JSON.stringify(searchRecords));
+
+                    //Render the page once records are fetched
+                    res.render('profile', {
+                        community_url: COMMUNITY_URL,
+                        app_id: APP_ID,
+                        callback_url: OAUTH_CALLBACK_URL,
+                        background: BG_FAKE,
+                        static_asset_url: STATIC_ASSET_URL,
+                        contactRecords: contactRecords,
+                        bookingRecords: bookingRecords,
+                        searchRecords: searchRecords,
+                        wishes: wishes
+                    }) 
+        
+                });
+        
+            });
+    
+        });
 
     });
+
 }); 
 
 app.get('/_callback', function(req, res){ 
